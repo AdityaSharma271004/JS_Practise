33cbb612a757fdd19f897c0f0036c41ba2bc7eaa
Aditya Sharma <147606124+AdityaSharma271004@users.noreply.github.com>
2025-07-15 17:21:50 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+let name = "adi"
+let age = 19
+let isLoggedIn = false
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+
+// object
+
+console.log(typeof "adi");//it gives the type of the variable
+
+console.log(typeof null);//op/- object
+console.log(typeof undefined);//op - undefined
