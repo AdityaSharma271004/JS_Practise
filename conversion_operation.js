0ad56aaa67c4ed5f36d07a55fd06bdeb313f484c
Aditya Sharma <147606124+AdityaSharma271004@users.noreply.github.com>
2025-07-17 15:00:36 +0530

    Add files via upload

---
+++ conversion_operation.js
@@ -0,0 +1,43 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number (score);
+
+// // console.log(typeof valueInNumber);
+let age = "33abc"
+let valueInNumber1 = Number (age);
+// console.log(valueInNumber1);//NaN
+
+//"33"  => 33
+// "33abc" => NaN
+// true =>1; false =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+// 1 => true ; 0=> false
+// " " => false
+// "adi" => true
+
+//************************* Operations */
+
+let value = 3
+let negValue  = -value
+//console.log(negValue);
+
+let str = "adi";
+let str2 = " sir";
+let str3 = str + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1  + 2 + "2");
+
+
+
+
